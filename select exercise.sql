26af11832d0cdf5952aac731378640ed9d6ea125
GeorgeHansen <nicholasfhansen@gmail.com>
2014-09-04 09:25:35 +0200

    sql exercise 4/9/2014

---
+++ select exercise.sql
@@ -0,0 +1,12 @@
+SELECT name from customers
+SELECT name, last_name from customers 
+SELECT name, last_name, phone, email from customers
+SELECT email, phone from customers
+SELECT * from customers
+SELECT name from customers where last_name = "Hansen"
+SELECT name, last_name from customers where name = "Per"
+SELECT * from customers where phone = 82647351
+SELECT name, phone from customers where email = "bent@kea.dk"
+SELECT id from customers where email = "pave@kea.dk"
+SELECT * from customers where name = "alchohol"
+SELECT name, last_name from customers where name = "claus"
