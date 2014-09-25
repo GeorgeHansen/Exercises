5050e9e260634216589381faa594b2433f80bf94
GeorgeHansen <nicholasfhansen@gmail.com>
2014-09-25 22:48:31 +0200

    adding sql exercise 9

---
+++ ex9HotelThing.sql
@@ -0,0 +1,38 @@
+SELECT * FROM hotel, room; -- could add booking and guest. Depends what you mean by full details
+
+SELECT * FROM hotel,room WHERE city LIKE"%London%";
+
+SELECT guestName, guestAddress FROM guest WHERE guestAddress LIKE "%London%"
+ORDER BY guestName DESC;
+
+SELECT * FROM room WHERE type IN ('d', 'f') AND price <= 40
+ORDER BY price; -- of course there aren't going to be any :p
+
+SELECT * FROM booking WHERE dateTo is null;
+
+SELECT COUNT(hotelNo) FROM hotel;
+
+SELECT AVG(price) FROM room;
+
+SELECT SUM(price) FROM room Where type = 'd'; -- this is assuming the rooms are filled of course
+
+SELECT DISTINCT COUNT(guestNo) FROM booking WHERE dateTo LIKE "%08%"; 
+-- you probably want to be less lazy to make sure this isnt 2008 or something. I'm guessing the __ thing would do it
+
+SELECT hotelNo, count(roomNo) FROM ROOM
+GROUP BY hotelNo; -- I wonder if there is an easy way to list the hotel name and the number of rooms they have
+
+-- don't know. Referencing different tables? How to do?
+
+INSERT INTO hotel VALUES (6,'Ritzy Biscuit','Copenhagen');
+
+UPDATE room SET price = price * 1.05; -- not sure why its being a bitch. Safe mode. Oh well
+
+DELETE FROM hotel WHERE hotelNO = 6;
+
+
+
+
+
+
+
